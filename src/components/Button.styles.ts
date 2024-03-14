855e7b0a5e2cb3f9f87dc760cec4702c00f84484
Carlos <carlos.msv@hotmail.com>
2024-03-14 02:29:45 -0300

    :fire:remove unused code

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,16 +1,9 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
 interface ButtonContainerProps {
   variant: ButtonVariant
-}
-
-const buttonVariants = {
-  primary: 'purple',
-  secondary: 'orange',
-  danger: 'red',
-  success: 'green'
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`

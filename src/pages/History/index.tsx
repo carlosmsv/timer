61ab99c23da9df262b03255e2286bb05b09bdb5e
Carlos <carlos.msv@hotmail.com>
2024-04-16 01:58:00 -0300

    :lipstick::sparkles:Status component

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -19,32 +19,34 @@ export function History() {
             <tr>
               <td>Task 1</td>
               <td>10 min</td>
-              <td>2 months ago</td>
-              <td>Finished</td>
+              <td>5 minutes ago</td>
+              <td>
+                <Status statusColor="yellow">In Progress</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task 1</td>
+              <td>15 min</td>
+              <td>1 month ago</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
             </tr>
             <tr>
               <td>Task 1</td>
               <td>10 min</td>
               <td>2 months ago</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="red">Interrupted</Status>
+              </td>
             </tr>
             <tr>
               <td>Task 1</td>
-              <td>10 min</td>
-              <td>2 months ago</td>
-              <td>Finished</td>
-            </tr>
-            <tr>
-              <td>Task 1</td>
-              <td>10 min</td>
-              <td>2 months ago</td>
-              <td>Finished</td>
-            </tr>
-            <tr>
-              <td>Task 1</td>
-              <td>10 min</td>
-              <td>2 months ago</td>
-              <td>Finished</td>
+              <td>30 min</td>
+              <td>3 months ago</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
             </tr>
           </tbody>
         </table>
